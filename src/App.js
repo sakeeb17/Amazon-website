f92f86e716caa1b8a93e9e6fb7f8cc7c63f9be14
Sakeeb Gadyal <mdsakeebgadyal@gmail.com>
2024-07-27 16:37:12 +0530

    amazon website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './header'
+import Banner from './banner';
+import Footer from './footer';
+import Content from './content';
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <Content/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
